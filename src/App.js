00ff63551bee2c30f7db365e889805ec61ea8062
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-10-25 12:27:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="heading">Welcome to React.js</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum omnis quae, magni saepe vitae incidunt blanditiis impedit quaerat beatae id voluptatibus nihil molestias officia molestiae, asperiores aliquid dolorem eos illum dolores dignissimos nisi magnam!</p>
+    </div>
+  );
+}
+
+export default App;
