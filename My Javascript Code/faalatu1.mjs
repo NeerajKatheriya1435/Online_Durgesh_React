98bd9652546bf81128ee5ed017a210371c053706
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-10-25 14:19:19 +0530

    day 5 react code

---
+++ My Javascript Code/faalatu1.mjs
@@ -0,0 +1,5 @@
+const str1 = "Nitya";
+const str2 = "Abhishek";
+const str3 = "Neeraj";
+
+export default { str1, str2, str3 };
---
+++ My Javascript Code/faalatu2.mjs
@@ -0,0 +1,8 @@
+// import { str1, str2, str3 } from "./faalatu1.mjs";
+import name123 from "./faalatu1.mjs";
+
+console.log(name123.str1);
+console.log(name123.str2);
+// console.log(str2);
+// console.log(str3);
+
