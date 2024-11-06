b87da3a6cd0ffda6e484a012a4efb16889e0601c
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-11-06 14:09:40 +0530

    day 8 clear and find methods

--- src/App.js
+++ src/App.js
@@ -8,12 +8,26 @@ import Navbar from "./components/Navbar"
 function App() {
 
   const [text, setText] = useState('')
+  const [email, setEmail] = useState([])
   const handleChange = (e) => {
     setText(e.target.value);
   }
   const handleUpperCase = () => {
     let newText = text.toUpperCase();
     setText(newText);
+  }
+  const handleLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+  const handleClearText = () => {
+    setText('');
+  }
+
+  const handleEmail = () => {
+    const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+    const totalEmails = text.match(emailRegex);
+    setEmail(totalEmails);
   }
   return (
     <>
@@ -25,8 +39,14 @@ function App() {
           <textarea onChange={handleChange} className="form-control" value={text} placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: '100px' }}></textarea>
         </div>
         <button onClick={handleUpperCase} className="btn btn-success my-2">Convert To UpperCase</button>
+        <button onClick={handleLowerCase} className="btn btn-success my-2 mx-2">Convert To LowerCase</button>
+        <button onClick={handleClearText} className="btn btn-success my-2 mx-2">Clear Text</button>
+        <button onClick={handleEmail} className="btn btn-success my-2 mx-2">Find Emails</button>
       </div>
       {/* <Navbar menuComp1={menuItem1} menuComp2={menuItem2} /> */}
+      <div className="container">
+        {email.map((elem) => (<div>{elem}</div>))}
+      </div>
     </>
   );
 }
